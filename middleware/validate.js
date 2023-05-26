e1afe0809c037d73a7d619b271a0cf12b456ec89
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2023-05-25 20:13:11 -0600

    PUT and DELETE functions

---
+++ middleware/validate.js
@@ -0,0 +1,25 @@
+const validator = require('../helpers/validate');
+
+const saveContact = (req, res, next) => {
+    const validationRule = {
+        "s1item1": 'required|string',
+        "s1item2": 'required|string',
+        "s1item3": 'required|string',
+        "s1item4": 'required|string'
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        if(!status) {
+            res.status(412).send({
+                success: false, 
+                message: 'Validation failed',
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
+
+module.exports = {
+    saveContact
+};
